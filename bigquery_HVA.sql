a61cde7e0906508b9a32ff12da72ca772d86e2b9
kh7393 <kevin.ho.7393@gmail.com>
2017-09-11 11:29:42 +0800

    v1

--- bigquery_HVA.sql
+++ bigquery_HVA.sql
@@ -2,16 +2,21 @@ SELECT
   fullVisitorId AS vid,
   CONCAT(fullVisitorId,"-",STRING(visitId)) AS sid,
   date,
-  MAX(IF(hits.eventInfo.eventAction = 'youtube', 1,0)) AS youtubeCheck,
+  # query for total events - youtube
+  SUM(IF(hits.eventInfo.eventCategory = 'youtube', 1,0)) AS youtubeEvents,
+  # query whether the session has youtube event or not [unique event]
+  MAX(IF(hits.eventInfo.eventCategory = 'youtube', 1,0)) AS youtubeCheck,
   # counts as unique events
   # query for total events - ecommerce impressions
   SUM(IF(REGEXP_MATCH(hits.eventInfo.eventCategory, r'ecommerce') AND hits.eventInfo.eventAction = 'impression', 1, 0)) as EcommerceImpressionEvents,
   # SUM(IF(hits.eventInfo.eventCategory LIKE '%ecommerce%' AND hits.eventInfo.eventAction = 'impression', 1, 0)) as EcommerceImpressionEvents,
+  # query whether the session has ecommerce impression event or not [unique event]
   MAX(IF(hits.eventInfo.eventCategory = 'ecommerce' AND hits.eventInfo.eventAction = 'impression', 1,0)) AS ecommerceImpressionCheck,
   # search check: whether the session has flight search or not
   MAX(IF(REGEXP_MATCH(hits.page.pagePath, r'/vbook/actions/(selectflights|selectitinerary|(mobi/|)createitinerary)($|\?)'),1,0)) AS flightSearchCheck,
   # trans check: whether the session has transaction or not
   MAX(IF(totals.transactions IS NULL, 0,1)) AS transCheck,
+  # query tables by date range
  FROM
   TABLE_DATE_RANGE([125557395.ga_sessions_], TIMESTAMP('20170210'), TIMESTAMP('20170211')) as Table1
   # don't group by visitor or session id if it's date-specific
