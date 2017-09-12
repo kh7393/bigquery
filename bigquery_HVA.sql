fe8432514cf814ced11d0b832a46e791a9f45b72
kh7393 <kevin.ho.7393@gmail.com>
2017-09-12 10:52:11 +0800

    v1

--- bigquery_HVA.sql
+++ bigquery_HVA.sql
@@ -39,6 +39,38 @@ SELECT
 
 
 
+# criteria for storefront
+CASE
+      WHEN REGEXP_MATCH(hits.page.hostname, r'grabaseat\.co\.nz') THEN 'GAS-NZ'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'(airn(z|ewzealand)|taxi|airpoints)\.co\.nz') THEN 'NZ'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.co\.uk') THEN 'UK'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.com\.au') THEN 'AUS'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.ca') THEN 'Canada'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.cn') THEN 'China'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'(airnewzealand\.com\.cn|cn\.airnewzealand\.com)$') THEN 'China EN'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.co\.jp') THEN 'Japan EN'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.jp') THEN 'Japan'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.com\.sg') THEN 'Singapore'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.com\.hk') THEN 'Hong Kong EN'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.hk') THEN 'Hong Kong'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.in') THEN 'India'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.co\.id') THEN 'Indonesia'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'en\.airnewzealand-ar\.com') THEN 'Argentina EN'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand-ar\.com') THEN 'Argentina'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.mx') THEN 'Mexico'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.com\.co') THEN 'Columbia'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.de$') THEN 'Germany'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'en\.airnewzealand-br\.com') THEN 'Brazil EN'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand-br\.com') THEN 'Brazil'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.fr') THEN 'France'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.eu') THEN 'Europe'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.pf') THEN 'French Polynesia'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'airnewzealand\.com\.vn') THEN 'Vietnam'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'pacificislands\.airnewzealand\.com$') THEN 'Pacific Islands'
+      WHEN REGEXP_MATCH(hits.page.hostname, r'(www|flightbookings|m|hotels)\.(airnewzealand|airnz)\.com$') THEN 'US'
+      ELSE NULL END AS storefront
+
+
 
   SELECT
   MAX(CASE WHEN customDimensions.index = 82 THEN customDimensions.value ELSE NULL END) WITHIN RECORD as customer_id,
